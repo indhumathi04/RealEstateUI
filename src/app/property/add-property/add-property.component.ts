e31ebaff8153ad9c9231fe15da175b18db6deb3e
indhumathi04 <indhumathivijay04@gmail.com>
2022-11-05 15:47:15 +0530

    card changes

--- src/app/property/add-property/add-property.component.ts
+++ src/app/property/add-property/add-property.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormBuilder, NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import {TabsetComponent} from 'ngx-bootstrap/tabs'
 import { Ipropertybase } from 'src/app/models/iproperty-base';
